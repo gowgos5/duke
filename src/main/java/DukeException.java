f92bcc339764b207fe11293625c5907096538f9c
gowgos5 <gowgos5@gmail.com>
2019-08-28 01:54:37 +0800

    Use Exceptions to handle errors

---
+++ src/main/java/DukeException.java
@@ -0,0 +1,10 @@
+public class DukeException extends Exception {
+    public static final String EXCEPTION_UNKNOWN_COMMAND = "I do not understand this command.";
+    public static final String EXCEPTION_EMPTY_DESCRIPTION = "The description of this command cannot be empty.";
+    public static final String EXCEPTION_EMPTY_DATETIME = "The date/time argument of this command cannot be empty";
+    public static final String EXCEPTION_TASKS_OUTOFBOUNDS = "There is no such task in your current list.";
+
+    public DukeException(String message) {
+        super(message);
+    }
+}
