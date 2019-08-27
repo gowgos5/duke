1106c8f9813f04d6ef62029043aa134ac4df8f16
gowgos5 <gowgos5@gmail.com>
2019-08-27 23:38:33 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     private static final String DIVIDER_LINE = "    ____________________________________________________________";
     private static final String INDENTATION_MESSAGE = "     ";
-    private static final String MESSAGE_GREET = "Hello! I'm Duke\n" + INDENTATION_MESSAGE + "What can I do for you?";
-    private static final String MESSAGE_EXIT = "Bye. Hope to see you again soon!";
+    private static final String MESSAGE_GREET = INDENTATION_MESSAGE + "Hello! I'm Duke\n" +
+            INDENTATION_MESSAGE + "What can I do for you?";
+    private static final String MESSAGE_EXIT = INDENTATION_MESSAGE + "Bye. Hope to see you again soon!";
+    private static final String MESSAGE_ADD = INDENTATION_MESSAGE + "added: ";
 
     public static void main(String[] args) {
+        List<String> userMessages = new ArrayList<>();
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -16,21 +22,31 @@ public class Duke {
 
         // Greet user
         System.out.println(DIVIDER_LINE);
-        System.out.println(INDENTATION_MESSAGE + MESSAGE_GREET);
+        System.out.println(MESSAGE_GREET);
         System.out.println(DIVIDER_LINE + "\n");
 
         Scanner scanner = new Scanner(System.in);
         boolean exitFlag = false;
 
         while (!exitFlag) {
-            String userCommand = scanner.nextLine();
+            String userMessage = scanner.nextLine();
 
             System.out.println(DIVIDER_LINE);
-            if (userCommand.equals("bye")) {
-                System.out.println(INDENTATION_MESSAGE + MESSAGE_EXIT);
-                exitFlag = true;
-            } else {
-                System.out.println(INDENTATION_MESSAGE + userCommand);
+            switch (userMessage) {
+                case "list":
+                    int i = 1;
+                    for (String message : userMessages) {
+                        System.out.println(INDENTATION_MESSAGE + i++ + ". " + message);
+                    }
+                    break;
+                case "bye":
+                    System.out.println(MESSAGE_EXIT);
+                    exitFlag = true;
+                    break;
+                default:
+                    userMessages.add(userMessage);
+                    System.out.println(MESSAGE_ADD + userMessage);
+                    break;
             }
             System.out.println(DIVIDER_LINE + "\n");
         }
